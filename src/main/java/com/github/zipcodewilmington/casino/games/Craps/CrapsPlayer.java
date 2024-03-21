1248cad979f66a4e34105dbce398bca5f3db5a93
Matt Kramer <matthew123003@gmail.com>
2024-03-21 18:19:04 -0400

    Small changes

--- src/main/java/com/github/zipcodewilmington/casino/games/Craps/CrapsPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/Craps/CrapsPlayer.java
@@ -33,8 +33,8 @@ public class CrapsPlayer implements PlayerInterface {
     }
 
     @Override
-    public <SomeReturnType> SomeReturnType play() {
-
-        return null;
+    public String play() {
+        craps.run();
+        return "Thank you for playing.";
     }
 }
